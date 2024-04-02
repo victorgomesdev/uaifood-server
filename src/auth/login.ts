365cac46f0e531ee6320cd167e9cf86436e5ebb0
Victor <victorgomesnog123@gmail.com>
2024-04-02 16:09:42 -0300

    Criando rotas e middlewares

---
+++ src/auth/login.ts
@@ -0,0 +1,6 @@
+import { verify } from 'jsonwebtoken'
+import { Request, Response, NextFunction} from 'express'
+
+export default async function loginMiddleware(req: Request, res: Response, next: NextFunction){
+    
+}
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,12 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import loginMiddleware from './auth/login'
+
+const server = express()
+server.use(bodyParser.json())
+
+//Login de usuário
+server.post('/login', loginMiddleware)
 
 
+server.listen(3000, ()=> console.log('SERVER RUNNING'))
