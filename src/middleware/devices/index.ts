28f49c9640d431bdeb583e3cb6adeb240a743c8e
Victor <victorgomesnog123@gmail.com>
2024-06-21 15:14:47 -0300

    Rota de listas

--- src/middleware/devices/index.ts
+++ src/middleware/devices/index.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express'
 import createDeviceMiddleware from './create_device'
 import remoteControlMiddleware from './control'
+import listDevices from './list'
 
 const deviceController = Router()
 
 deviceController.post('/device/create', createDeviceMiddleware)
 deviceController.post('/device/control', remoteControlMiddleware)
-
+deviceController.post('/device/list', listDevices)
 export default deviceController
